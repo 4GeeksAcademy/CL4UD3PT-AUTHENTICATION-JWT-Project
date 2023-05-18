65b94ae8f8a35218cb3dff9fa4dd75a9c0c96f39
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-05-18 07:07:10 +0000

    models created; .env BACKEND_URL added

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_type_id = db.Column(db.Integer, db.ForeignKey(
+        'user_type.id'), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +18,12 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "user_type": self.user_type
             # do not serialize the password, its a security breach
         }
+
+
+class UserType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String, unique=True, nullable=False)
+    user_types = db.relationship("User", backref="type")
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,28 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, UserType
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/signup', methods=['POST'])
+def user_signup():
+    body = request.json
+    new_user = User(
+        email=body["email"],
+        password=body["password"],
+        is_active=body["is_active"],
+        type_id=body["user_type"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    response_body = {"Users": "User created successfully"}
 
     return jsonify(response_body), 200
+
+
+@api.route('/hello', methods=['GET'])
+def handle_hello():
+    return jsonify({"msg": "hello"})
